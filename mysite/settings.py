9c0ffc3a7576691ab07c54ca5856d7bf654a9484
maria mercedes <mcmechitascantillo@gmail.com>
2019-01-24 10:49:40 -0500

    Cambie el HTML para la página.

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {di
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, static)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
